b8935e992e1db5b262d5e949181ce090041600cb
YozoraSky <john.hsiao@hualiteq.com>
2019-05-24 16:23:22 +0800

    更新成batchUpdate寫法

--- ivr-schedule/src/main/java/com/ctbcbank/ivr/schedule/batch/DetailLog.java
+++ ivr-schedule/src/main/java/com/ctbcbank/ivr/schedule/batch/DetailLog.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,30 +39,34 @@ public class DetailLog {
 	public void run(){
 		try {
 			int data=0;
-			long time = System.currentTimeMillis();
+			//抓前一天(一天有86400000毫秒)
+			long time = System.currentTimeMillis()-86400000;
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 			Date now = new Date(time);
-			//比對資料夾內有多少符合日期的txt  若是Linux系統需去除"C:"
 			int NumFile = checkFile(sdf.format(now), batchDlogProperties.getLogPath());
 			//每個資料的處理
+			List<String> sqlArray = new ArrayList<String>();
 			for(int i = 0;i <= (NumFile-1); i++) {
 				String readFileName = sdf.format(now) + "." + i + ".txt";
-				//若是Linux系統需去除"C:"
 				FileReader fr = new FileReader(batchDlogProperties.getLogPath() + "detailLog." + readFileName);
 				BufferedReader reader = new BufferedReader(fr);
-					//讀取檔案內容資料和資料庫處理
-					while (reader.ready()) {
-						String line = reader.readLine();
-						String sql = DES._DecryptByDES(
-								line.substring(line.indexOf("ivr_detail_log - ")+17,line.indexOf("#")),
-								keyproperties.getKey());
-						jdbcTemplate.execute(sql);
-						data++;
-						}
+				//讀取檔案內容資料和資料庫處理
+				while (reader.ready()) {
+					String line = reader.readLine();
+					String sql = DES._DecryptByDES(line.substring(line.indexOf("ivr_detail_log - ")+17,line.indexOf("#")),keyproperties.getKey());
+					sqlArray.add(sql);
+					if(sqlArray.size()>=5000) {
+						jdbcTemplate.batchUpdate(sqlArray.toArray(new String[0]));
+						sqlArray.clear();
+					}
+					data++;
+				}
 				fr.close();
 			}
+			jdbcTemplate.batchUpdate(sqlArray.toArray(new String[0]));
+			sqlArray.clear();
 			logger.info("Read " + NumFile + " Folder");
-			logger.info(data + " data columns are affected");
+			logger.info(data + " sql columns are executed");
 		} 
 		catch (Exception e) {
 			logger.info(e.toString());
