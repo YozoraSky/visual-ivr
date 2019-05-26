7f38cd96c123283f5347200f740ed53b7850e894
YozoraSky <john.hsiao@hualiteq.com>
2019-05-26 22:28:15 +0800

    修改中...

--- ivr-schedule/src/main/java/com/ctbcbank/ivr/schedule/batch/DetailLog.java
+++ ivr-schedule/src/main/java/com/ctbcbank/ivr/schedule/batch/DetailLog.java
@@ -3,6 +3,7 @@ package com.ctbcbank.ivr.schedule.batch;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.LineNumberReader;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -27,6 +28,7 @@ import com.ctbcbank.ivr.schedule.properties.KeyProperties;
 @PropertySource(value = { "classpath:batchDlog.properties" })
 public class DetailLog {
 	private Logger logger = LoggerFactory.getLogger("batch_Dlog");
+	private final String insertSQL = "INSERT INTO IVRScheduleStatus() VALUES()";
 	@Autowired
 	private BatchDlogProperties batchDlogProperties;
 	@Autowired
@@ -38,7 +40,7 @@ public class DetailLog {
 	@Scheduled(cron="${batchDlog.cron.msg}")
 	public void run(){
 		try {
-			int data=0;
+			int totalSqlNum=0;
 			//抓前一天(一天有86400000毫秒)
 			long time = System.currentTimeMillis()-86400000;
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
@@ -46,27 +48,43 @@ public class DetailLog {
 			int NumFile = checkFile(sdf.format(now), batchDlogProperties.getLogPath());
 			//每個資料的處理
 			List<String> sqlArray = new ArrayList<String>();
-			for(int i = 0;i <= (NumFile-1); i++) {
+			String tempFileNum = (String) jdbcTemplate.queryForMap(insertSQL).get("FileNum");
+			String tempLineNum = (String) jdbcTemplate.queryForMap(insertSQL).get("LineNum");
+			int fileNum = 0;
+			int lineNum = 0;
+			if(tempFileNum!=null) {
+				fileNum = Integer.parseInt(tempFileNum);
+			}
+			if(tempLineNum!=null) {
+				lineNum = Integer.parseInt(tempLineNum);
+			}
+			for(int i = fileNum;i <= (NumFile-1); i++) {
+				jdbcTemplate.execute(insertSQL); //insert i into fileNum
 				String readFileName = sdf.format(now) + "." + i + ".txt";
 				FileReader fr = new FileReader(batchDlogProperties.getLogPath() + "detailLog." + readFileName);
-				BufferedReader reader = new BufferedReader(fr);
+				BufferedReader bufferedReader = new BufferedReader(fr);
+				LineNumberReader reader = new LineNumberReader(bufferedReader);
 				//讀取檔案內容資料和資料庫處理
 				while (reader.ready()) {
 					String line = reader.readLine();
-					String sql = DES._DecryptByDES(line.substring(line.indexOf("ivr_detail_log - ")+17,line.indexOf("#")),keyproperties.getKey());
-					sqlArray.add(sql);
-					if(sqlArray.size()>=5000) {
-						jdbcTemplate.batchUpdate(sqlArray.toArray(new String[0]));
-						sqlArray.clear();
+					if(reader.getLineNumber()>lineNum) {
+						String sql = DES._DecryptByDES(line.substring(line.indexOf("ivr_detail_log - ")+17,line.indexOf("#")),keyproperties.getKey());
+						sqlArray.add(sql);
+						if(sqlArray.size()>=5000) {
+							jdbcTemplate.batchUpdate(sqlArray.toArray(new String[0]));
+							jdbcTemplate.execute(sql); // insert reader.getLineNumber() into LineNum
+							sqlArray.clear();
+						}
+						totalSqlNum++;
 					}
-					data++;
 				}
 				fr.close();
 			}
 			jdbcTemplate.batchUpdate(sqlArray.toArray(new String[0]));
 			sqlArray.clear();
+			jdbcTemplate.execute(insertSQL); // insert reader.getLineNumber() into LineNum && insert status "s"
 			logger.info("Read " + NumFile + " Folder");
-			logger.info(data + " sql columns are executed");
+			logger.info(totalSqlNum + " sql columns are executed");
 		} 
 		catch (Exception e) {
 			logger.info(e.toString());
