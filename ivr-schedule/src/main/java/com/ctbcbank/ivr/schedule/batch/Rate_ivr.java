eceee91c1ea202938d8f00cd2161869c3abf2b67
YozoraSky <john.hsiao@hualiteq.com>
2019-07-04 15:41:23 +0800

    修改Rate_ivr的SQL語句從 replace字串改成透過 NamedParameterJdbcTemplate 直接讀取Map參數

--- ivr-schedule/src/main/java/com/ctbcbank/ivr/schedule/batch/Rate_ivr.java
+++ ivr-schedule/src/main/java/com/ctbcbank/ivr/schedule/batch/Rate_ivr.java
@@ -19,6 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -32,29 +34,30 @@ import com.ctbcbank.ivr.schedule.sftp.FTPUtil;
 public class Rate_ivr {
 	private Logger logger = LoggerFactory.getLogger("rate_ivr");
 	@Autowired
-	@Qualifier("ivrConfigJdbcTemplate")
-	private JdbcTemplate jdbcTemplate;
+	@Qualifier("ivrConfigNamedParameterJdbcTemplate")
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	@Autowired
 	private RateProperties rateProperties;
-	
-	@Scheduled(cron="${rate_ivr.cron.msg}")
-	public void run(){
+
+	@Scheduled(cron = "${rate_ivr.cron.msg}")
+	public void run() {
 		String password = new String(Base64.getDecoder().decode(rateProperties.getPassword()));
-		FTPUtil ftp = new FTPUtil(rateProperties.getHost(),rateProperties.getUsername(),password,21);
+		FTPUtil ftp = new FTPUtil(rateProperties.getHost(), rateProperties.getUsername(), password, 21);
 		try {
 			ftp.login();
 			long time = System.currentTimeMillis();
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HHmmss");
 			Date now = new Date(time);
 			String saveFileName = "rate_ivr(" + sdf.format(now) + ").txt";
-			ftp.downloadFile(rateProperties.getDirectory(), rateProperties.getDownloadFile(), rateProperties.getSavePath() + "/" + saveFileName);
+			ftp.downloadFile(rateProperties.getDirectory(), rateProperties.getDownloadFile(),
+					rateProperties.getSavePath() + "/" + saveFileName);
 			logger.info("ftp download success");
 			ftp.logout();
-			
+
 			List<Map<String, String>> interestRateCode = getInterestRateCode();
-			
+
 			InputStream input = new FileInputStream(rateProperties.getSavePath() + "/" + saveFileName);
-			InputStreamReader inputStreamReader = new InputStreamReader(input,"big5");
+			InputStreamReader inputStreamReader = new InputStreamReader(input, "big5");
 			BufferedReader reader = new BufferedReader(inputStreamReader);
 			String productName;
 			String magicCode;
@@ -64,61 +67,61 @@ public class Rate_ivr {
 			String line;
 			byte[] temp;
 			int[] status;
-			ArrayList<String> sqlArray = new ArrayList<String>();
-			while((line = reader.readLine())!=null) {
+			List<MapSqlParameterSource> batchArgsForInsert = new ArrayList<MapSqlParameterSource>();
+			while ((line = reader.readLine()) != null) {
 				temp = line.getBytes("big5");
-				//magicCode後兩碼的空白不要去掉，因為之後要和csv檔的magicCode去做對比
-				magicCode = new String(Arrays.copyOfRange(temp, 1, 1+11),"big5");
-				productName = new String(Arrays.copyOfRange(temp, 26, 26+40),"big5").trim();
-				rate = new String(Arrays.copyOfRange(temp, 86, 86+7),"big5").trim();
-				sdate = new String(Arrays.copyOfRange(temp, 196, 196+8),"big5").trim();
-				edate = new String(Arrays.copyOfRange(temp, 204, 204+8),"big5").trim();
-				if(!productName.equals("")) {
-					for(int i=0;i<interestRateCode.size();i++) {
-						if(magicCode.equals(interestRateCode.get(i).get("MagicCode").replace("-", " ")) && 
-						   productName.equals(interestRateCode.get(i).get("ProductName"))) {
-//							String uuid = UUID.randomUUID().toString();
-							String sql = rateProperties.getInterestRateSql();
-							sql = sql.replace("@CurrencyId", interestRateCode.get(i).get("CurrencyId"))
-									 .replace("@ProductCode", interestRateCode.get(i).get("ProductCode"))
-									 .replace("@RateType", interestRateCode.get(i).get("RateType"))
-									 .replace("@TermType", interestRateCode.get(i).get("TermType"))
-									 .replace("@Term", interestRateCode.get(i).get("Term"))
-									 .replace("@AmountType", interestRateCode.get(i).get("AmountType"))
-									 .replace("@ProductName", productName)
-									 .replace("@Rate", rate)
-									 .replace("@StartDate", sdate)
-									 .replace("@EndDate", edate)
-									 .replace("@IVR", interestRateCode.get(i).get("IVR"));
-							sqlArray.add(sql);
+				// magicCode後兩碼的空白不要去掉，因為之後要和csv檔的magicCode去做對比
+				magicCode = new String(Arrays.copyOfRange(temp, 1, 1 + 11), "big5");
+				productName = new String(Arrays.copyOfRange(temp, 26, 26 + 40), "big5").trim();
+				rate = new String(Arrays.copyOfRange(temp, 86, 86 + 7), "big5").trim();
+				sdate = new String(Arrays.copyOfRange(temp, 196, 196 + 8), "big5").trim();
+				edate = new String(Arrays.copyOfRange(temp, 204, 204 + 8), "big5").trim();
+				if (!productName.equals("")) {
+					for (int i = 0; i < interestRateCode.size(); i++) {
+						if (magicCode.equals(interestRateCode.get(i).get("MagicCode").replace("-", " "))
+								&& productName.equals(interestRateCode.get(i).get("ProductName"))) {
+							MapSqlParameterSource parameters = new MapSqlParameterSource();
+							parameters.addValue("CurrencyId", interestRateCode.get(i).get("CurrencyId"));
+							parameters.addValue("ProductCode", interestRateCode.get(i).get("ProductCode"));
+							parameters.addValue("RateType", interestRateCode.get(i).get("RateType"));
+							parameters.addValue("TermType", interestRateCode.get(i).get("TermType"));
+							parameters.addValue("Term", interestRateCode.get(i).get("Term"));
+							parameters.addValue("AmountType", interestRateCode.get(i).get("AmountType"));
+							parameters.addValue("ProductName", productName);
+							parameters.addValue("Rate", rate);
+							parameters.addValue("StartDate", sdate);
+							parameters.addValue("EndDate", edate);
+							parameters.addValue("IVR", interestRateCode.get(i).get("IVR"));
+							batchArgsForInsert.add(parameters);
 						}
 					}
 				}
 			}
-			status = jdbcTemplate.batchUpdate(sqlArray.toArray(new String[0]));
-			int success = 0,fail = 0;
-			for(int i:status) {
-				if(i==1)
+			status = namedParameterJdbcTemplate.batchUpdate(rateProperties.getInterestRateSql(),
+					batchArgsForInsert.toArray(new MapSqlParameterSource[0]));
+			int success = 0, fail = 0;
+			for (int i : status) {
+				if (i == 1)
 					success++;
 				else
 					fail++;
 			}
-			String sql = rateProperties.getBatchSql().replace("@FilePath", saveFileName)
-													 .replace("@SuccessCount", String.valueOf(success))
-													 .replace("@FailCount", String.valueOf(fail));
-			jdbcTemplate.execute(sql);
+			Map<String, Object> params = new HashMap<String, Object>();
+			params.put("FilePath", saveFileName);
+			params.put("SuccessCount", String.valueOf(success));
+			params.put("FailCount", String.valueOf(fail));
+			namedParameterJdbcTemplate.update(rateProperties.getBatchSql(), params);
 			reader.close();
-			logger.info("insert data number : " + sqlArray.size());
-		}
-		catch(Exception e) {
-			logger.error("---ERROR--- : ",e);
+			logger.info("insert data number : " + batchArgsForInsert.size());
+		} catch (Exception e) {
+			logger.error("---ERROR--- : ", e);
 		}
 		logger.info("#$$%%%%$$#");
 	}
-	
-	private List<Map<String, String>> getInterestRateCode() throws Exception{
+
+	private List<Map<String, String>> getInterestRateCode() throws Exception {
 		InputStream input = new FileInputStream(rateProperties.getInterestRateCodePath());
-		InputStreamReader inputStreamReader = new InputStreamReader(input,"big5");
+		InputStreamReader inputStreamReader = new InputStreamReader(input, "big5");
 		BufferedReader reader = new BufferedReader(inputStreamReader);
 		String line;
 		String[] interestRateCode;
@@ -127,11 +130,11 @@ public class Rate_ivr {
 		Map<String, String> tempMap;
 		int count = 0;
 		int num = 0;
-		while((line = reader.readLine())!=null) {
+		while ((line = reader.readLine()) != null) {
 			interestRateCode = line.split(",");
-			if(count == 0)
+			if (count == 0)
 				key = interestRateCode;
-			else{
+			else {
 				tempMap = new HashMap<String, String>();
 				tempMap.put(key[num], interestRateCode[num++]);
 				tempMap.put(key[num], interestRateCode[num++]);
@@ -150,9 +153,6 @@ public class Rate_ivr {
 			count++;
 		}
 		reader.close();
-//		for(int i=0;i<interestRateCodeMap.size();i++) {
-//			System.out.println(interestRateCodeMap.get(i));
-//		}
 		return interestRateCodeMap;
 	}
 }
